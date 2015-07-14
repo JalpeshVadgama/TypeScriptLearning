cde1f49f5a69c76e9d26fa427a3217742ce115e6
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-07-14 23:35:52 +0530

    Added Car typescript file.

---
+++ TypeScriptLearning/TypeScriptLearning/Scripts/Car.ts
@@ -0,0 +1,22 @@
+﻿class car{
+
+    engine: string 
+
+    constructor(engine: string) {
+        this.engine = engine;
+    }
+
+    start() {
+        alert('Engine started: ' + this.engine);
+    }
+
+    stop() {
+        alert('Engine Stopped: ' + this.engine);
+    }
+}
+
+window.onload = function () {
+    var car = new car('v8');
+    car.start();
+    car.stop();
+};
